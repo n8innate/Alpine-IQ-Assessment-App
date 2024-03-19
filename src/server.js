0f8f427b13f94d51f252e2538fbd8aebe0358104
Nathaniel A <63274597+n8innate@users.noreply.github.com>
2024-03-19 17:06:13 -0400

    src/server:  Added Endpoint for GET /contacts - testing

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const Contact = require('./models/Contact.js');
 
 // Parse incoming JSON 
 app.use(express.json());
@@ -10,8 +11,17 @@ app.get('/', (req, res) => {
     res.send("Hello New Job! :)")
 });
 
+app.get('/contacts', async (req, res) => {
+    try {
+        const contacts = await Contact.findAll();
+        res.json(contacts);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
+
 // Start the Server...
 app.listen(port, () => {
     console.log(`Server running on port ${port}...`)
-})
-
+});
