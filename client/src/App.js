2c497bb94bcad66c38fab4d420c7657901469815
Nathaniel A <63274597+n8innate@users.noreply.github.com>
2024-03-19 19:19:15 -0400

    client/App:  Updated App to fetch clients from the server - Need to update components to use contacts

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import AddContactForm from './components/AddContactForm.js';
+import ContactsList from './components/ContactsList.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [contacts, setContacts] = useState([]);
+
+    const fetchContacts = () => {
+        fetch('/contacts')
+            .then((res) => res.json())
+            .then((data) => setContacts(data))
+            .catch((e) => console.error("Error: ", e))
+    };
+
+    useEffect(() => {
+        fetchContacts();
+    }, [])
+
+    return (
+        <div>
+            <h1>Contact Directory</h1>
+            <AddContactForm contacts={contacts} onContactAdded={fetchContacts} />
+            <ContactsList contacts={contacts} />
+        </div>
+    );
 }
 
 export default App;
