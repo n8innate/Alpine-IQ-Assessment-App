60d30439c3c9f42959fc79c4184f1e95f36ec0c2
Nathaniel A <63274597+n8innate@users.noreply.github.com>
2024-03-19 19:44:49 -0400

    ContactsList: Clean Up

--- client/src/components/ContactsList.js
+++ client/src/components/ContactsList.js
@@ -3,15 +3,13 @@ import React from 'react';
 function ContactsList({ contacts }) {
     return (
         <ul>
-            {
-                contacts.map((contact) => (
-                    <li key={contact.id}>
-                        Name: {contact.name},
-                        Email: {contact.email},
-                        Address: {contact.address ? contact.address : 'N/A'}
-                    </li>
-                ))
-            }
+            {contacts.map((contact) => (
+                <li key={contact.id}>
+                    Name: {contact.name},
+                    Email: {contact.email},
+                    Address: {contact.address ? contact.address : 'N/A'}
+                </li>
+            ))}
         </ul>
     );
 };
