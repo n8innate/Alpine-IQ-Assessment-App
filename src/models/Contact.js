87c179e04d81518b72704ce817f71c8a1d38fa92
Nathaniel A <63274597+n8innate@users.noreply.github.com>
2024-03-19 16:55:37 -0400

    src/models/Contact:  Added queries - testing

--- src/models/Contact.js
+++ src/models/Contact.js
@@ -1,7 +1,18 @@
-const Contact = {
-    findAll: async () => {},
+const db = require('../db');
 
-    create: async () => {},
+const Contact = {
+    findAll: async () => {
+        const { rows } = await db.query('SELECT * FROM contacts');
+        return rows;
+    },
+
+    create: async ({name, email, address}) => {
+        const { rows } = await db.query(
+            'INSERT INTO contacts (name, email, address) VALUES ($1, $2, $3) RETURNING *',
+            [name, email, address]
+        );
+        return rows;
+    },
 }
 
 module.exports = Contact;
